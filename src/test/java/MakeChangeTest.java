e3a847b6f501dd3ecc4720c27d45a5a0ae48ce27
Rebecca Tang <rebecca.tang@axxes.com>
2024-03-18 11:39:44 +0100

    added test to check for different coins to return, fails

--- src/test/java/MakeChangeTest.java
+++ src/test/java/MakeChangeTest.java
@@ -10,26 +10,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MakeChangeTest {
 
     CoinHelper coinHelper = new CoinHelper();
+    CoinValidator coinValidator = new CoinValidator(0.25f, 0.05f);
 
-    @Test
-    public void given_moreCoinThanNeededInMachine_when_selectProduct_then_RemainingCoinInCoinReturn() {
-        CoinValidator coinValidator = new CoinValidator(0.25f, 0.05f);
+    private VendingMachine getBasicVendingMachine() {
         Map<Products, Integer> availableProducts = new HashMap<>(Map.ofEntries(
                 Map.entry(Products.COLA, 10),
                 Map.entry(Products.CANDY, 5),
                 Map.entry(Products.CHIPS, 1)
         ));
 
-        VendingMachine vendingMachine = new VendingMachine(coinValidator, availableProducts);
+        return new VendingMachine(coinValidator, availableProducts);
+    }
+
+    private double calculateSumOfReturn(List<Coin> returnedCoins) {
+        return returnedCoins.stream()
+                .map(coinValidator::validateCoin)
+                .mapToDouble(validatedCoin -> validatedCoin != null ? validatedCoin.getValue() : 0)
+                .sum();
+    }
+
+    @Test
+    public void given_oneQuarterMoreThanProductPrice_when_selectProduct_then_RemainingCoinReturned() {
+        VendingMachine vendingMachine = getBasicVendingMachine();
         vendingMachine.insert(coinHelper.getQuarter());
         vendingMachine.insert(coinHelper.getQuarter());
         vendingMachine.insert(coinHelper.getQuarter());
         vendingMachine.selectProduct(Products.CHIPS);
 
-        List<Coin> returnedCoins = vendingMachine.checkCoinReturn();
-        assertEquals(0.25, returnedCoins.stream()
-                                    .map(coinValidator::validateCoin)
-                                    .mapToDouble(validatedCoin -> validatedCoin != null ? validatedCoin.getValue() : 0)
-                                    .sum());
+        assertEquals(0.25, calculateSumOfReturn(vendingMachine.checkCoinReturn()));
+    }
+
+    @Test
+    public void given_multipleDifferentCoinsMoreThanProductPrice_when_selectProduct_then_RemainingCoinReturned() {
+        VendingMachine vendingMachine = getBasicVendingMachine();
+        vendingMachine.insert(coinHelper.getQuarter());
+        for (int i = 0; i < 10 ; i++) {
+            vendingMachine.insert(coinHelper.getDime());
+        }
+        vendingMachine.insert(coinHelper.getNickel());
+        vendingMachine.insert(coinHelper.getNickel());
+
+        assertEquals(0.35, calculateSumOfReturn(vendingMachine.checkCoinReturn()));
     }
 }
